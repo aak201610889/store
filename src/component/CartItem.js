10bb8c8e2dfe5a9ec8b2d735277790414eaee99e
ayham-kattan <ayhamkattan1@gmail.com>
2021-11-26 12:39:28 +0200

    clear-cartItem

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import './CartItem.css'
-// import Modal from 'react-modal'
-// import Zoom from 'react-reveal'
-function CartItem({ product, Sort, Size, addToCart }) {
-  // const [model, setmodel] = useState(null)
 
-  // const openModal = (product) => {
-  //   setmodel({product})
-  // }
-  // const closeModal = () => {
-  //   setmodel(null)
-  // }
-  // console.log(model);
+function CartItem({ product, Sort, Size, addToCart }) {
 
   return (
     <div className="cartitem">
@@ -28,23 +18,18 @@ function CartItem({ product, Sort, Size, addToCart }) {
         
         .filter((item) => item.availableSizes.indexOf(Size) > 0)
         .map((product) => (
-          
-          <div key={product.id}
-            //onClick={() => openModal(product)}
-          >
+          <div key={product.id}>
               <img
                 src={product.image}
                 alt={product.title}
                 width="250px"
                 height="320px"
               />
-
               <div className="sub__title">
                 <p>{product.price}</p>
                 <p>{product.title}</p>
                 <p>{product.availableSizes.indexOf(Size)}</p>
               </div>
-
               <button
                 onClick={() => addToCart(product)}
                 className="additem__btn"
@@ -54,14 +39,7 @@ function CartItem({ product, Sort, Size, addToCart }) {
             </div>
           
         ))}
-      {/* {product && (
-        <Modal isOpen={false} onRequestClose={() => closeModal}>
-          <Zoom>
-            <button onClick={() => openModal(product)}>X</button>
-            <img src={product.image} alt={product.title} />
-          </Zoom>
-        </Modal>
-      )} */}
+  
 
 
     </div>
